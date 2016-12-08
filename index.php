0f33414fc146fcdfc3b635a811d8604f17e5b75c
Rafael Nery <rafanery0@gmail.com>
2016-12-08 12:29:05 -0200

    Update index.php

--- index.php
+++ index.php
@@ -41,14 +41,12 @@ if($update->has('message')) {
             case '/php':
                 $telegram->sendPhoto([
                     'chat_id' => getenv('CHAT_ID'),
-                    //'photo' => $inputFile->open(),
                     'photo' => 'http://php.net/manual/en/images/c0d23d2d6769e53e24a1b3136c064577-php_logo.png',
                     'caption' => 'PHP!!!'
                 ]);
                 break;
             case '/renan':
             case '/scar':
-
                 $telegram->sendPhoto([
                     'chat_id' => getenv('CHAT_ID'),
                     //'photo' => $inputFile->open(),
